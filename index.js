d54e3506c3217fdff452f6ebfc1320049d19843f
saulius <sjsaulius@gmail.com>
2023-07-19 21:46:32 +0300

    add makeImage to project and finished

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const form = document.querySelector("#searchForm");
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
-  const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-
-  const img = document.createElement("img");
-  img.src.res.data[5].show.image.medium;
-  document.body.append(img);
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  makeImages(res.data);
+  // const img = document.createElement("img");
+  // img.src = res.data[5].show.image.medium;
+  // document.body.append(img);
 });
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("img");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
