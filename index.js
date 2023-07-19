107870c36628188a393e070c7d42fd953fe8d1e7
saulius <sjsaulius@gmail.com>
2023-07-19 15:48:00 +0300

    add img source but doesnt work

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
   const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-  console.log(res.data);
+
+  const img = document.createElement("img");
+  img.src.res.data[5].show.image.medium;
+  document.body.append(img);
 });
